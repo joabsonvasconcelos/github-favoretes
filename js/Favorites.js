7678e320657cb8056f035b896deeec5d4c89305e
Joabson Vasconcelos <119760277+joabsonvasconcelos@users.noreply.github.com>
2024-01-22 08:26:44 -0300

    uptade

---
+++ js/Favorites.js
@@ -0,0 +1,9 @@
+export class Favorites {
+    constructor(root) {
+        this.root = document.querySelector(root)
+    }
+}
+export class FavoritesView extends Favorites {
+    constructor(root) {}
+        super(root)
+}
